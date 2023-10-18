d08d1f087e63d4af20cba8d21fa25624570f3964
紫芋仙子 <zhengfeifan@papegames.net>
2023-10-18 17:38:31 +0800

    if part & quiz1 finished

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(q: i32) -> i32 {
+    let cost = if q > 40 {
+        1
+    } else {
+        2
+    };
+    cost * q
+}
 
 // Don't modify this function!
 #[test]
